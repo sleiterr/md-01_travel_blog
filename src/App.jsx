edab665c8f32f0b051c9524f9dd316bd9612f799
Oleg Troian <sleit@mac.lan>
2026-01-13 19:30:46 +0100

    feat: add rating statistics logic, about section and reusable layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Navbar from "./components/header/NavBar.jsx";
 import Hero from "./components/hero/Hero.jsx";
+import StatisticsPanel from "./components/StatisticsPanel/StatisticsPanel.jsx";
+import About from "./components/about/About.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <StatisticsPanel />
+      <About />
     </>
   );
 }
